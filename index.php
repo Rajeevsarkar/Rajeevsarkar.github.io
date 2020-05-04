1469abd382dfede00e2721176c886020cfd50d5f
Rajeevsarkar <rajeevsarkar27@gmail.com>
2020-05-04 08:46:18 +0530

    Revert "Delete index.php"
    This reverts commit ecb4de4f6405106bdee7dfb0bf15bdb85df5ab5a.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>haha</h1>
+<div>
+	<?php
+	echo "<h1>haha</h1>";
+	$var1=4;
+	$var2=9;
+	echo "<br>";
+
+	echo $var1+$var2;
+
+	echo var_dump($var1>$var2);
+	
+	?>
+</div>
+</body>
+</html>
